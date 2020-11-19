5f07b0f8accb41cbeda3cbce8002d7933b5ed280
caovanchien123 <caovanchien.tdc2018@gmail.com>
2020-11-19 10:35:10 +0700

    Lan dau

---
+++ app/src/main/java/com/example/hethongthuenha/Login/LoginPresenter.java
@@ -0,0 +1,16 @@
+package com.example.hethongthuenha.Login;
+
+public class LoginPresenter implements LoginContract.Presenter {
+    private LoginContract.View view;
+
+
+    public LoginPresenter(LoginContract.View view) {
+
+    }
+
+    @Override
+    public void LoginWithEmail(String email, String password) {
+
+    }
+
+}
